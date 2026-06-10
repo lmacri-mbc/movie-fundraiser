9dbaf9deec1c7a3294ecac6ca3945acc4deae24f
lmacri-mbc <lee.macri@mbc.school.nz>
2026-06-11 10:53:00 +1200

    Update instructions

--- instructions.py
+++ instructions.py
@@ -1,16 +1,21 @@
 def show_instructions():
-    print('''
-For each ticket holder enter…
--	Their name
--	Their age
--	The payment method (cash/credit)
+    """Display instructions for this app."""
+    instructions = "For each ticket holder enter…"
+    instructions += '''
+    -	Their name
+    -	Their age
+    -	The payment method (cash/credit)
 
-The program will record the ticket sale and calculate the ticket cost (and the profit).
+    The program will record the ticket sale and calculate
+    the ticket cost (and the profit).
 
-Once you have either sold all the tickets or entered the exit code (‘xxx’), the program will display the ticket sales info and write the data to a text file.
+    Once you have sold all the tickets, the program
+    will display the ticket sales info and write the data
+    to a text file when the End Program button is pressed.
 
-It will also choose one lucky ticket holder who wins the draw (their ticket is free).
-    ''')
+    It will also choose one lucky ticket holder who wins
+    the draw (their ticket is free).
+    '''
 
 
 show_instructions()
--- main_v1.py
+++ main_v1.py
@@ -64,10 +64,9 @@ def show_instructions():
     The program will record the ticket sale and calculate
     the ticket cost (and the profit).
 
-    Once you have either sold all the tickets or entered
-    the exit code (‘xxx’), the program
+    Once you have sold all the tickets, the program
     will display the ticket sales info and write the data
-    to a text file.
+    to a text file when the End Program button is pressed.
 
     It will also choose one lucky ticket holder who wins
     the draw (their ticket is free).
